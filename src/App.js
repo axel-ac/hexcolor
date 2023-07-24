07f670811905cb02e963856f90fe044e11eabc2f
axel-ac <abdullahceylan.axel@gmail.com>
2023-07-24 22:48:22 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [hex, setHex] = useState("#ffffff");
+
+  function randomHex() {
+    const randomhex = "#" + Math.floor(Math.random() * 16777721).toString(16)
+    setHex(randomhex)
+  }
+
   return (
-    <div className="App">
-      <h1>------</h1>
-      <button>Click for New Background</button>
-      <button>Copy the Hex Value</button>
+    <div className="App" style={{ backgroundColor: `${hex}` }}>
+      <h1>{hex}</h1>
+      <button onClick={randomHex}>Click for New Background</button>
+      <button onClick={() => navigator.clipboard.writeText(hex)}>
+        Copy the Hex Value
+      </button>
       <h4>
-        <a href="https://github.com/axel-ac" target='_blank'>Visit my Github Profile</a>
+        <a href="https://github.com/axel-ac" target="_blank">
+          Visit my Github Profile
+        </a>
       </h4>
     </div>
   );
