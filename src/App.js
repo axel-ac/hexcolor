d0846bad01180b8501f47de38fc9ffe7cc44e91f
axel-ac <abdullahceylan.axel@gmail.com>
2023-07-24 22:02:21 +0300

    css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Hi!
+    <div className="App">
+      <h1>------</h1>
+      <button>Click for New Background</button>
+      <button>Copy the Hex Value</button>
+      <h4>
+        <a href="https://github.com/axel-ac" target='_blank'>Visit my Github Profile</a>
+      </h4>
     </div>
   );
 }
